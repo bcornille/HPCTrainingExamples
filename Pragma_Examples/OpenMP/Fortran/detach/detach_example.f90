63c8829cc3fe9f832c244751a158c32c45ae7741
Brian Cornille <Brian.Cornille@amd.com>
2024-07-15 14:25:23 -0700

    Work in progress. Debugging event value in callback.

--- Pragma_Examples/OpenMP/Fortran/detach/detach_example.f90
+++ Pragma_Examples/OpenMP/Fortran/detach/detach_example.f90
@@ -1,25 +1,26 @@
 program openmp_rocblas
-  use hipfort, only: hipcheck, hipcreatestream, hipstreamaddcallback, &
-                   & hipblascheck, hipblascreate, hipblassetstream, hipblasdaxpy
+  use hipfort, only: hipsuccess, hipstreamcreate, hipstreamaddcallback
+  use hipfort_hipblas, only: hipblascreate, hipblassetstream, hipblasdaxpy
+  use hipfort_check, only: hipcheck, hipblascheck
   use iso_c_binding, only: c_ptr, c_funptr, c_loc, c_funloc, c_f_pointer
-  use iso_fortran_env, only: int32, real64,
+  use iso_fortran_env, only: int32, real64
   use omp_lib, only: omp_get_wtime, omp_event_handle_kind, omp_fulfill_event
   implicit none
 
   integer(int32) :: outer, inner, stream
   integer(int32) :: nouter = 4, ninner = 16*1024, nstreams = 4
-  integer(omp_event_handle_kind), allocatable :: events(:)
+  integer(omp_event_handle_kind), allocatable, target :: events(:)
   real(real64), PARAMETER :: a = 2.0
   type(c_ptr), allocatable :: streams(:), handles(:)
-  real(real64), allocatable :: x(:,:), y(:,:)
+  real(real64), allocatable, target :: x(:,:), y(:,:)
   real(real64) :: timings(5)
 
   call read_input()
-  allocate(events(0:nstreams-1), streams(0:nstreams-1), handles(nouter))
+  allocate(streams(nstreams), handles(nouter), events(nouter))
   allocate(x(nouter, ninner), y(nouter, ninner))
 
   do stream = 0, nstreams-1
-    call hipcheck(hipcreatestream(streams(stream)))
+    call hipcheck(hipstreamcreate(streams(stream)))
   end do
   do outer = 1, nouter
     call hipblascheck(hipblascreate(handles(outer)))
@@ -29,6 +30,7 @@ program openmp_rocblas
 
   timings(1) = omp_get_wtime()
 
+  write(*,*) "Start first loop"
   do outer = 1, nouter
     stream = mod(outer,nstreams)
     !$omp target teams distribute parallel do nowait depend(out:streams(stream))
@@ -37,20 +39,47 @@ program openmp_rocblas
       y(outer,inner) = 0.25_real64
     end do
   end do
+  write(*,*) "End first loop"
 
   timings(2) = omp_get_wtime()
 
+  write(*,*) "Start second loop"
   do outer = 1, nouter
-    stream = mod(outer,nstreams)
-    !$omp task depend(inout:streams(stream)) detach(events(stream))
-    call hipblascheck(hipblassetstream(handles(outer), streams(stream)))
-    call hipblascheck(hipblasdaxpy(handles(outer), ninner, a, c_loc(x(outer,1)), 1, c_loc(y(outer,1)), 1))
-    call hipcheck(hipstreamaddcallback(streams(stream), c_funloc(callback), c_loc(events(stream)), 0))
-    !$omp end task
+    if (nstreams > 1) then
+      stream = mod(outer,nstreams) + 1
+    else
+      stream = 1
+    end if
+    block
+      interface
+        subroutine callback(stream, status, cb_dat) bind(c)
+          use iso_c_binding, only: c_ptr
+          type(c_ptr), value :: stream, cb_dat
+          integer(kind(hipsuccess)), value :: status
+        end subroutine callback
+      end interface
+      integer(omp_event_handle_kind) :: event
+      write(*,*) "Before task: ", event
+      write(*,*) "  location: ", loc(event)
+      !$omp task depend(inout:streams(stream)) detach(event)
+      call hipblascheck(hipblassetstream(handles(outer), streams(stream)))
+      !$omp target data use_device_addr(x,y)
+      call hipblascheck(hipblasdaxpy(handles(outer), ninner, a, c_loc(x(outer,1)), 1, c_loc(y(outer,1)), 1))
+      !$omp end target data
+      write(*,*) "In task: ", event
+      write(*,*) "  location: ", loc(event)
+      call hipcheck(hipstreamaddcallback(streams(stream), c_funloc(callback), c_loc(event), 0))
+      !$omp end task
+      events(nouter) = event
+      write(*,*) "After task: ", event
+      write(*,*) "  location: ", loc(event)
+    end block
   end do
+  write(*,*) "End second loop"
 
   timings(3) = omp_get_wtime()
 
+  write(*,*) "Start third loop"
   do outer = 1, nouter
     stream = mod(outer,nstreams)
     !$omp target teams distribute parallel do nowait depend(in:streams(stream))
@@ -58,9 +87,11 @@ program openmp_rocblas
       y(outer, inner) = y(outer, inner) - a*x(outer, inner)
     end do
   end do
+  write(*,*) "End third loop"
 
   timings(4) = omp_get_wtime()
 
+  write(*,*) "Waiting"
   !$omp taskwait
 
   timings(5) = omp_get_wtime()
@@ -79,32 +110,40 @@ contains
     integer(int32) :: nargs, iarg
     character(len=64) :: option, value
     nargs = command_argument_count()
-    do iarg = 1, nargs
+    do iarg = 1, nargs, 2
       call get_command_argument(iarg,option)
       select case(option)
       case("-n","--num-regions")
-        iarg = iarg + 1
-        call get_command_argument(iarg, value)
+        call get_command_argument(iarg+1, value)
         read(value,'(i8)') nouter
       case("-s","--num-streams")
-        iarg = iarg + 1
-        call get_command_argument(iarg, value)
+        call get_command_argument(iarg+1, value)
         read(value,'(i8)') nstreams
-      case("-w","--size-work")
-        iarg = iarg + 1
-        call get_command_argument(iarg, value)
+      case("-w","--work-size")
+        call get_command_argument(iarg+1, value)
         read(value,'(i8)') ninner
       case default
         call print_usage()
         error stop "Unrecognized command "//trim(option)
+      end select
     end do
   end subroutine read_input
+
+  subroutine print_usage()
+    write(*,*) "Usage: detach_example <args>"
+    write(*,*) "  -n,--num-regions    number of outer loop trips"
+    write(*,*) "  -w,--work-size      number of inner loop trips"
+    write(*,*) "  -s,--num-streams    number of streams to utilize"
+  end subroutine
 
   subroutine callback(stream, status, cb_dat) bind(c)
     type(c_ptr), value :: stream, cb_dat
     integer(kind(hipsuccess)), value :: status
     integer(omp_event_handle_kind), pointer :: event
     call c_f_pointer(cb_dat, event)
+    write(*,*) "Staus: ", status, "/", hipsuccess
+    write(*,*) "In callback: ", event
+      write(*,*) "  location: ", loc(event)
     call omp_fulfill_event(event)
   end subroutine callback
 
