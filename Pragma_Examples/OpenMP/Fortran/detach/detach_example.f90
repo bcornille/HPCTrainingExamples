f8989840adb550fcfcb02ba82b182efcd2fca96a
Brian Cornille <Brian.Cornille@amd.com>
2024-07-16 06:50:14 -0700

    Removed race conditions.

--- Pragma_Examples/OpenMP/Fortran/detach/detach_example.f90
+++ Pragma_Examples/OpenMP/Fortran/detach/detach_example.f90
@@ -1,6 +1,6 @@
 program openmp_rocblas
-  use hipfort, only: hipsuccess, hipstreamcreate, hipstreamaddcallback
-  use hipfort_hipblas, only: hipblascreate, hipblassetstream, hipblasdaxpy
+  use hipfort, only: hipsuccess, hipstreamcreate, hipstreamaddcallback, hipstreamdestroy
+  use hipfort_hipblas, only: hipblascreate, hipblassetstream, hipblasdaxpy, hipblasdestroy
   use hipfort_check, only: hipcheck, hipblascheck
   use iso_c_binding, only: c_ptr, c_funptr, c_loc, c_funloc, c_f_pointer
   use iso_fortran_env, only: int32, real64
@@ -9,17 +9,21 @@ program openmp_rocblas
 
   integer(int32) :: outer, inner, stream
   integer(int32) :: nouter = 4, ninner = 16*1024, nstreams = 4
-  integer(omp_event_handle_kind), allocatable, target :: events(:)
+  integer(omp_event_handle_kind), allocatable :: events(:)
+  integer(omp_event_handle_kind) :: event
   real(real64), PARAMETER :: a = 2.0
   type(c_ptr), allocatable :: streams(:), handles(:)
   real(real64), allocatable, target :: x(:,:), y(:,:)
   real(real64) :: timings(5)
 
   call read_input()
+  write(*,*) "Work size: ", ninner
+  write(*,*) "Num tasks: ", nouter
+  write(*,*) "Num streams: ", nstreams
   allocate(streams(nstreams), handles(nouter), events(nouter))
   allocate(x(nouter, ninner), y(nouter, ninner))
 
-  do stream = 0, nstreams-1
+  do stream = 1, nstreams
     call hipcheck(hipstreamcreate(streams(stream)))
   end do
   do outer = 1, nouter
@@ -30,7 +34,7 @@ program openmp_rocblas
 
   timings(1) = omp_get_wtime()
 
-  write(*,*) "Start first loop"
+  !write(*,*) "Start first loop"
   do outer = 1, nouter
     stream = mod(outer,nstreams)
     !$omp target teams distribute parallel do nowait depend(out:streams(stream))
@@ -39,47 +43,38 @@ program openmp_rocblas
       y(outer,inner) = 0.25_real64
     end do
   end do
-  write(*,*) "End first loop"
+  !write(*,*) "End first loop"
 
   timings(2) = omp_get_wtime()
 
-  write(*,*) "Start second loop"
+  !write(*,*) "Start second loop"
   do outer = 1, nouter
     if (nstreams > 1) then
       stream = mod(outer,nstreams) + 1
     else
       stream = 1
     end if
-    block
-      interface
-        subroutine callback(stream, status, cb_dat) bind(c)
-          use iso_c_binding, only: c_ptr
-          type(c_ptr), value :: stream, cb_dat
-          integer(kind(hipsuccess)), value :: status
-        end subroutine callback
-      end interface
-      integer(omp_event_handle_kind) :: event
-      write(*,*) "Before task: ", event
-      write(*,*) "  location: ", loc(event)
-      !$omp task depend(inout:streams(stream)) detach(event)
-      call hipblascheck(hipblassetstream(handles(outer), streams(stream)))
-      !$omp target data use_device_addr(x,y)
-      call hipblascheck(hipblasdaxpy(handles(outer), ninner, a, c_loc(x(outer,1)), 1, c_loc(y(outer,1)), 1))
-      !$omp end target data
-      write(*,*) "In task: ", event
-      write(*,*) "  location: ", loc(event)
-      call hipcheck(hipstreamaddcallback(streams(stream), c_funloc(callback), c_loc(event), 0))
-      !$omp end task
-      events(nouter) = event
-      write(*,*) "After task: ", event
-      write(*,*) "  location: ", loc(event)
-    end block
+    !write(*,*) "Before task: ", event
+    !write(*,*) "  location: ", loc(event)
+    !$omp task depend(inout:streams(stream)) detach(event)
+    call hipblascheck(hipblassetstream(handles(outer), streams(stream)))
+    !$omp target data use_device_addr(x,y)
+    call hipblascheck(hipblasdaxpy(handles(outer), ninner, a, c_loc(x(outer,1)), 1, c_loc(y(outer,1)), 1))
+    !$omp end target data
+    !write(*,*) "In task: ", event
+    !write(*,*) "  location: ", loc(event)
+    events(outer) = event
+    call hipcheck(hipstreamaddcallback(streams(stream), c_funloc(callback), c_loc(events(outer)), 0))
+    !$omp end task
+    !write(*,*) "After task: ", event
+    !write(*,*) "  location: ", loc(event)
   end do
-  write(*,*) "End second loop"
+  !write(*,*) "End second loop"
+  !write(*,*) "Events :", events
 
   timings(3) = omp_get_wtime()
 
-  write(*,*) "Start third loop"
+  !write(*,*) "Start third loop"
   do outer = 1, nouter
     stream = mod(outer,nstreams)
     !$omp target teams distribute parallel do nowait depend(in:streams(stream))
@@ -87,16 +82,22 @@ program openmp_rocblas
       y(outer, inner) = y(outer, inner) - a*x(outer, inner)
     end do
   end do
-  write(*,*) "End third loop"
+  !write(*,*) "End third loop"
 
   timings(4) = omp_get_wtime()
 
-  write(*,*) "Waiting"
+  !write(*,*) "Waiting"
   !$omp taskwait
 
   timings(5) = omp_get_wtime()
 
   !$omp target exit data map(delete:x,y)
+  do outer = 1, nouter
+    call hipblascheck(hipblasdestroy(handles(outer)))
+  end do
+  do stream = 1, nstreams
+    call hipcheck(hipstreamdestroy(streams(stream)))
+  end do
 
   write(*,*) "First loop submit time: ", timings(2) - timings(1)
   write(*,*) "Second loop submit time: ", timings(3) - timings(2)
@@ -121,7 +122,7 @@ contains
         read(value,'(i8)') nstreams
       case("-w","--work-size")
         call get_command_argument(iarg+1, value)
-        read(value,'(i8)') ninner
+        read(value,'(i64)') ninner
       case default
         call print_usage()
         error stop "Unrecognized command "//trim(option)
@@ -141,9 +142,9 @@ contains
     integer(kind(hipsuccess)), value :: status
     integer(omp_event_handle_kind), pointer :: event
     call c_f_pointer(cb_dat, event)
-    write(*,*) "Staus: ", status, "/", hipsuccess
-    write(*,*) "In callback: ", event
-      write(*,*) "  location: ", loc(event)
+    !write(*,*) "Staus: ", status, "/", hipsuccess
+    !write(*,*) "In callback: ", event
+    !write(*,*) "  location: ", loc(event)
     call omp_fulfill_event(event)
   end subroutine callback
 
